9e81789804f0f2ba876660f677c5d0566f3ca199
Anit <anittankandasamy@gmail.com>
2022-10-02 20:39:32 +0000

    setters and getters

--- class.js
+++ class.js
@@ -1,7 +1,6 @@
 class Pizza {
 
-  constructor(size, crust) {
-    this.size = size;
+  constructor(crust) {
     this.crust = crust;
     this.toppings = ["cheese"];
   }
@@ -9,17 +8,33 @@ class Pizza {
   addTopping(topping) {
     this.toppings.push(topping);
   }
+
+  set size(size) {
+    if (size === 's' || size === 'm' || size === 'l' ) {
+    this._size = size;
+  }
 }
 
-let pizza1 = new Pizza('large', 'thin');
-console.log(pizza1.toppings); //["cheese"]
+  get price() {
+    const basePrice = 10;
+    const toppingPrice = 2;
+    return basePrice + this.toppings.length * toppingPrice;
+  }
+}
+
+let pizza1 = new Pizza('medium', 'thin');
+// console.log(pizza1.toppings); //["cheese"]
 pizza1.addTopping("mushrooms");
 pizza1.addTopping("peppers");
-console.log(pizza1.toppings); //["cheese", "mushrooms", "peppers"]
+pizza1.size = 'm'; // m 
+pizza1.price;
+// console.log(pizza1.toppings); //["cheese", "mushrooms", "peppers"]
 console.log(pizza1);
 
-let pizza2 = new Pizza();
-console.log(pizza2.toppings); //["cheese"]
+let pizza2 = new Pizza('large', 'thin');
+// console.log(pizza2.toppings); //["cheese"]
 pizza2.addTopping("more cheese");
-console.log(pizza2.toppings); //["cheese", "more cheese"];
+pizza2.size = 'l'; // l
+pizza2.price;
+// console.log(pizza2.toppings); //["cheese", "more cheese"];
 console.log(pizza2);
